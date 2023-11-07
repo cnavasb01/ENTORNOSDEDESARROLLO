b9f5babb3cb3a20a0c57ee617e4a400183e736c9
cnavasb01 <cnavasb01@educarex.es>
2023-11-07 13:57:26 +0100

    Inicio proyecto

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class Holamundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+     System.out.println("Holaa");
+     System.out.println("Veremos si funciona esto");
+	}
+
+}
---
+++ HolaPersona.java
@@ -0,0 +1,16 @@
+public class Holapersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner mc= new Scanner (System.in);
+String nombre;
+
+System.out.println("Introduce tu nombre:");
+nombre=mc.next();
+System.out.println("Buenos días,"+nombre);
+
+		
+		
+	}
+
+}
